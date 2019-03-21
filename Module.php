bd3b328564aa12533dc7471438100fb56668fe87
nadine-shi <nadine@afterlogic.com>
2019-03-21 13:53:26 +0300

    Copyright

--- Module.php
+++ Module.php
@@ -12,7 +12,7 @@ namespace Aurora\Modules\MailChangePasswordHmailserverPlugin;
  *
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing AfterLogic Software License
- * @copyright Copyright (c) 2018, Afterlogic Corp.
+ * @copyright Copyright (c) 2019, Afterlogic Corp.
  *
  * @package Modules
  */
