bb305abbb9331b1f7992457d1d6d20b8f3137130
nadine-shi <nadinshi@gmail.com>
2022-05-26 12:32:29 +0300

    Do not change URL hash after reload

--- vue/manager.js
+++ vue/manager.js
@@ -3,19 +3,20 @@ import settings from '../../MailChangePasswordHmailserverPlugin/vue/settings'
 export default {
   moduleName: 'MailChangePasswordHmailserverPlugin',
 
-  requiredModules: [],
+  requiredModules: ['MailWebclient'],
 
   init (appData) {
     settings.init(appData)
   },
+
   getAdminSystemTabs () {
     return [
       {
-        tabName: 'mail-hmailserver-plugin',
-        title: 'MAILCHANGEPASSWORDHMAILSERVERPLUGIN.LABEL_HMAIL_SETTINGS_TAB',
-        component () {
-          return import('./components/HmailserverAdminSettings')
-        },
+        tabName: 'hmailserver',
+        tabTitle: 'MAILCHANGEPASSWORDHMAILSERVERPLUGIN.LABEL_HMAIL_SETTINGS_TAB',
+        tabRouteChildren: [
+          { path: 'hmailserver', component: () => import('./components/HmailserverAdminSettings') },
+        ],
       },
     ]
   },
