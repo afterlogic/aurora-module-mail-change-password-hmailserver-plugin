aabe65852f705312967f836e502f9db2a102265b
Vasil Sokolov <vasil@afterlogic.com>
2023-01-23 23:21:56 +0200

    Updated dates in copyright

--- Module.php
+++ Module.php
@@ -12,7 +12,7 @@ namespace Aurora\Modules\MailChangePasswordHmailserverPlugin;
  * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
- * @copyright Copyright (c) 2019, Afterlogic Corp.
+ * @copyright Copyright (c) 2023, Afterlogic Corp.
  *
  * @package Modules
  */
