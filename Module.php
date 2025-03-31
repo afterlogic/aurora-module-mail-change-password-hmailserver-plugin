c32362f990f1413e102ccf9d0ee93feec46b6ae4
sash04ek <a.ovcharov@gmail.com>
2025-03-31 15:22:31 +0300

    Refactoring of error code functionality

--- Module.php
+++ Module.php
@@ -8,6 +8,7 @@
 namespace Aurora\Modules\MailChangePasswordHmailserverPlugin;
 
 use Aurora\Modules\Mail\Models\MailAccount;
+use Aurora\System\Notifications;
 
 /**
  * Allows users to change passwords on their email accounts hosted by [hMailServer](https://www.hmailserver.com/).
@@ -204,10 +205,10 @@ class Module extends \Aurora\System\Module\AbstractModule
                         }
                     }
                 } catch (\Exception $oException) {
-                    throw new \Aurora\System\Exceptions\ApiException(\Aurora\System\Exceptions\Errs::UserManager_AccountNewPasswordUpdateError);
+                    throw new \Aurora\System\Exceptions\ApiException(Notifications::CanNotChangePassword);
                 }
             } else {
-                throw new \Aurora\System\Exceptions\ApiException(\Aurora\System\Exceptions\Errs::UserManager_AccountNewPasswordUpdateError);
+                throw new \Aurora\System\Exceptions\ApiException(Notifications::CanNotChangePassword);
             }
         }
         return $mResult;
